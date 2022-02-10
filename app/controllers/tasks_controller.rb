bed0d8c823f2557b248ba2cf03fccc63a3056d20
hanakidayo <hanapanaki@icloud.com>
2022-02-10 15:14:50 +0000

    Mysql構築

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,16 @@
+class TasksController < ApplicationController
+
+    def index
+        @tasks = Task.all
+    end
+        
+    def show
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def edit
+    end
+end
