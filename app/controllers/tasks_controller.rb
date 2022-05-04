bd294c9d062ff1a3a25ed7bc3d140fc51e8a9c3d
hanakidayo <hanapanaki@icloud.com>
2022-05-04 15:04:28 +0000

    最終形態

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,16 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
     def index
-      @tasks = Task.all
+      @tasks = Task.order(id: :desc).page(params[:page]).per(3)
     end
 
     def show
-      @task = Task.find(params[:id])
+      
     end
 
     def new
       @task = Task.new
-    end
-
-    def edit
-      @task = Task.find(params[:id])
     end
 
     def create
@@ -29,31 +26,33 @@ class TasksController < ApplicationController
       end
     end
     
-    def update
-    @task = Task.find(params[:id])
-
-    if @task.update(task_params)
-      flash[:success] = 'メッセージは正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'メッセージは更新されませんでした'
-      render :edit
+    def edit
     end
+    
+    def update
+      if @task.update(task_params)
+        flash[:success] = 'メッセージは正常に更新されました'
+        redirect_to @task
+      else
+        flash.now[:danger] = 'メッセージは更新されませんでした'
+        render :edit
+      end
     end
     
     def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
+      @task.destroy
 
-    flash[:success] = 'メッセージは正常に削除されました'
-    redirect_to tasks_url
+      flash[:success] = 'メッセージは正常に削除されました'
+      redirect_to tasks_url
     end
     
   private
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :title, :status)
     end
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
 end
-
-
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_145955) do
+ActiveRecord::Schema.define(version: 2022_04_19_143528) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "status"
   end
 
 end
